5308acd8c3000b4e9ee7da87081c2c89e7356461
Tanner Braun <tsbraun1891@gmail.com>
2018-11-10 18:01:42 -0500

    File pathing

--- src/GUI/DI.java
+++ src/GUI/DI.java
@@ -22,16 +22,25 @@ public class DI {
         backButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                MainFrame.init();
+                MainFrame.init();  // Return to main menu
             }
         });
         rollButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                int num = Integer.parseInt(numField.getText());
-                int sides = Integer.parseInt(sideField.getText());
-                int buff = Integer.parseInt(buffField.getText());
-                resLab.setText(String.valueOf(Dicey.Roll(num,sides,buff)));
+                String numf = numField.getText();
+                String sidef = sideField.getText();
+                String bufff = buffField.getText();
+                if(!(numf.isEmpty()) && !(sidef.isEmpty())){  //If fields left empty, do nothing
+                    int buff;
+                    if(bufff.isEmpty())
+                        buff = 0;
+                    else
+                        buff = Integer.parseInt(bufff);
+                    int num = Integer.parseInt(numf);
+                    int sides = Integer.parseInt(sidef);
+                    resLab.setText("Result: " + String.valueOf(Dicey.Roll(num,sides,buff)));
+                }
             }
         });
     }
