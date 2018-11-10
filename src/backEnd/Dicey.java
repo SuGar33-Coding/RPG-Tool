ad81d150e59dc7c5c9bf8c54d298d05b6997c074
Tanner Braun <tsbraun1891@gmail.com>
2018-11-10 14:57:21 -0500

    Dicey Fix

--- src/backEnd/Dicey.java
+++ src/backEnd/Dicey.java
@@ -4,7 +4,7 @@ public class Dicey {
     public static int Roll(int number, int sides, int buff){
         int total = 0;
         for(int i = 0; i < number; i++)
-            total += (int)(Math.random()*(sides-1)+1);
+            total += (int)(Math.random()*(sides)+1);
         total += buff;
         return total;
     }
