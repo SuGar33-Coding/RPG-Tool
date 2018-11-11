510bbd4dcdddc0946e1ccb7532f9d46e73dcdd4b
Tanner Braun <tsbraun1891@gmail.com>
2018-11-10 19:55:14 -0500

    Expanded Dicey beyond comprehension

---
+++ src/GUI/CharForm.java
@@ -0,0 +1,24 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CharForm {
+    public JPanel charPan;
+    private JPanel namePan;
+    private JPanel infoPan;
+    private JPanel statsPanel;
+    private JLabel player;
+    private JTextField playerF;
+    private JTextField charF;
+    private JLabel race;
+    private JLabel character;
+    private JTextField textField1;
+    private JTextField classF;
+    private JTextField levelF;
+    private JTextField alignmentF;
+    private JTextField xpF;
+    private JLabel classlab;
+    private JLabel level;
+    private JLabel alignment;
+    private JLabel xp;
+}
